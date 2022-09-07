61f357fc052eb10417e383dd06533939486cf32d
jeff kang <jeffyjkang@gmail.com>
2022-09-07 10:40:36 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const server = require("./api/server.js");
+// const server = require("./api/server.js");
 
-const port = 9000;
-server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
-});
+// const port = 9000;
+// server.listen(port, () => {
+//   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+// });
+
+// console.log(process.argv)
+// console.log(process.env)
+
+require('dotenv').config()
+const express = require('express')
+const server = express()
+
+server.get('/api/users', (req,res) => {
+  res.json([
+    {id: 1, username: 'foo'},
+    {id: 2, username: 'bar'},
+    {id: 3, username: 'baz'}
+  ])
+})
+const PORT = process.env.PORT || 8080
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
